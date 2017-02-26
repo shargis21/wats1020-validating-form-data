4fa36def5152828eea1d78c44ac2708c3c1d768b
shargis21 <shargis21@gmail.com>
2017-02-26 17:28:13 -0500

    modified css, index, and js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,43 @@
 
 // For this assignment you'll need to do a few things:
 // 1. Create a document ready handler.
+$(document).on('ready', function(){
 // 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
+      $('#order-form').validate({
+        submitHandler: function(form) {
+          // 3. Connect the validation object to an event handler tied to the submit button.
+            form.submit();
+        },
 
-// Refer to the `index.html` file for the validation rules that must be enforced.
+         rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128
+            },
+            "your-state": {
+                required: true,
+                maxlength: 2
+            },
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
+            }
+        }
+    });
+    // Tooltips
+    $('label span.glyphicon').tooltip();
+});
